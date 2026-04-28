ed3ad596dfaa892794b2e224f7d549dc4e7da628
Oleg-Blinov <olegb20070226@gmail.com>
2026-04-28 12:29:29 +0000

    update auth

--- backend/config.py
+++ backend/config.py
@@ -1,29 +1,31 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config():
-    SECRET_KEY= os.getenv('SECRET_KEY', 'super-secret-key')
-    LOGTO_CLIENT_ID  = os.getenv('LOGTO_CLIENT_ID')
-    LOGTO_CLIENT_SECRET = os.getenv('LOGTO_CLIENT_SECRET')
-    LOGTO_ISSUER = os.getenv('LOGTO_ISSUER')
-    LOGTO_REDIRECT_URI = os.getenv('LOGTO_REDIRECT_URI')
-    SESSION_TYPE = 'filesystem'
-    SESSION_FILE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'flask_session')
-    SESSION_COOKIE_DOMAIN = '127.0.0.1'
-    YOOKASSA_SHOP_ID = os.getenv('YOOKASSA_SHOP_ID')
-    YOOKASSA_SECRET_KEY = os.getenv('YOOKASSA_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
+    LOGTO_CLIENT_ID = os.getenv("LOGTO_CLIENT_ID")
+    LOGTO_CLIENT_SECRET = os.getenv("LOGTO_CLIENT_SECRET")
+    LOGTO_ISSUER = os.getenv("LOGTO_ISSUER")
+    LOGTO_REDIRECT_URI = os.getenv("LOGTO_REDIRECT_URI")
+    SESSION_TYPE = "filesystem"
+    SESSION_FILE_DIR = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "flask_session"
+    )
+    SESSION_COOKIE_DOMAIN = ".safe-contact.duckdns.org"
+    YOOKASSA_SHOP_ID = os.getenv("YOOKASSA_SHOP_ID")
+    YOOKASSA_SECRET_KEY = os.getenv("YOOKASSA_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
-    ALLOWED_EXTENSIONS = {'pdf', 'jpg', 'jpeg', 'png', 'doc', 'docx'}
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "uploads")
+    ALLOWED_EXTENSIONS = {"pdf", "jpg", "jpeg", "png", "doc", "docx"}
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5 MB
-    CALCOM_API_KEY = os.getenv('CALCOM_API_KEY')
-    CALCOM_API_URL = os.getenv('CALCOM_API_URL')
-    CALCOM_EVENT_TYPE_SLUG = os.getenv('CALCOM_EVENT_TYPE_SLUG')
-    CALCOM_WEBHOOK_SECRET = os.getenv('CALCOM_WEBHOOK_SECRET')
-
-    
+    CALCOM_API_KEY = os.getenv("CALCOM_API_KEY")
+    CALCOM_API_URL = os.getenv("CALCOM_API_URL")
+    CALCOM_EVENT_TYPE_SLUG = os.getenv("CALCOM_EVENT_TYPE_SLUG")
+    CALCOM_WEBHOOK_SECRET = os.getenv("CALCOM_WEBHOOK_SECRET")
